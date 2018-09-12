652350efb80fd483b4b952b3f29eb28fe4b14e72
Abraham Elmahrek <abraham@elmahrek.com>
2018-09-11 21:14:07 -0700

    Add atob and btoa polyfill

--- globals.js
+++ globals.js
@@ -3,9 +3,17 @@ global.process = require('process');
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
-  protocol: 'file:',
+  protocol: 'file:'
 };
 
 // Some modules expect userAgent to be a string
 global.navigator = global.navigator || {};
-global.navigator.userAgent = global.navigator.userAgent || "React Native";
+global.navigator.userAgent = global.navigator.userAgent || 'React Native';
+
+if (!global.btoa) {
+  global.btoa = require('base-64').encode;
+}
+
+if (!global.atob) {
+  global.atob = require('base-64').decode;
+}
